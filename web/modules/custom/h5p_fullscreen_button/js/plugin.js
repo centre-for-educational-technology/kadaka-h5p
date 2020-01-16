2dbd964ccdac6e918f5f4e45e53e2a54c8231965
leoste <35530198+leoste@users.noreply.github.com>
2020-01-16 12:31:43 +0200

    Fix bug
    Click event got applied to all buttons

--- web/modules/custom/h5p_fullscreen_button/js/plugin.js
+++ web/modules/custom/h5p_fullscreen_button/js/plugin.js
@@ -31,8 +31,8 @@ self.t = function(key, vars) {
     var type =  "fullscreen";
 
     $('ul.h5p-actions')
-    .append('<li class="h5p-button h5p-noselect h5p-' + type + '" role="button" tabindex="0" title="' + self.t(type + 'Description') + '">' + self.t(type) + '</li>')
-    .click(function() {
+    .append('<li class="h5p-button h5p-noselect h5p-' + type + '" role="button" tabindex="0" title="' + self.t(type + 'Description') + '">' + self.t(type) + '</li>');
+    $('ul.h5p-actions .h5p-' + type).click(function() {
       window.top.location.href = url;
     });
   })
